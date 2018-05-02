9cfbaba155b0042db73d96adb86a2453bdefbc27
Eric Luciano <ericluciano@MacBook-Pro-de-Eric.local>
2018-05-01 21:57:09 -0300

    correção de código e add prop-types

--- src/components/modalCurrency/modalCurrency.jsx
+++ src/components/modalCurrency/modalCurrency.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+/* eslint no-trailing-spaces: "error" */
+/* eslint class-methods-use-this: "error" */
+/* eslint-env es6 */
+
+import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
@@ -7,49 +12,65 @@ import { changeCripto, closeOrOpenModalCripto } from '../../reducers/criptoActio
 import { moedas } from '../../constantes/moedas';
 import { capitalize } from '../../utils/utils';
 
-class ModalCurrency extends React.Component{
-  constructor(props) {
-    super(props);
-  }
-
+class ModalCurrency extends React.Component {
   mountLi(props) {
-    const moedas = props || [];
-    const items = Object.keys(moedas).sort();
-    return items.map(moeda => (
-        <li>
-          <div className='link' onClick={() => this.props.changeCripto(moeda)}>
-            {capitalize(moeda)}
-          </div>
-        </li>
-      )
-    )
+    const moedasCorrentes = props || [];
+    const items = Object.keys(moedasCorrentes).sort();
+    return items.map((moeda, key) => (
+      <li key={moeda}>
+        <div
+          className="lnk outline-0"
+          onClick={() => this.props.changeCripto(moeda)}
+          onKeyPress={() => this.props.changeCripto(moeda)}
+          role="link"
+          tabIndex={key}
+        >
+          {capitalize(moeda)}
+        </div>
+      </li>
+    ));
   }
 
   render() {
-
     const { modalChangeCripto } = this.props;
 
-    return(
+    return (
       <div>
         <div className={`modal-currency ${modalChangeCripto}`}>
-          <div className={`close ${modalChangeCripto}`} onClick={() => this.props.closeOrOpenModalCripto('inactive')}>X</div>
+          <div
+            role="link"
+            tabIndex="0"
+            className={`close ${modalChangeCripto}`}
+            onClick={() => this.props.closeOrOpenModalCripto('inactive')}
+            onKeyPress={() => this.props.closeOrOpenModalCripto('inactive')}
+          >
+            X
+          </div>
           <div className="content">
             <ul>
               {this.mountLi(moedas)}
             </ul>
           </div>
         </div>
-        <div className={`bg-modal-currency ${modalChangeCripto}`}></div>
+        <div className={`bg-modal-currency ${modalChangeCripto}`} />
       </div>
-    )
+    );
   }
 }
 
+
 const mapStateToProps = state => ({
   moedaAtual: state.cripto.moedaAtual,
-  modalChangeCripto: state.cripto.modalChangeCripto });
+  modalChangeCripto: state.cripto.modalChangeCripto,
+});
 
-const mapDispatchToProps = dispatch =>
-bindActionCreators({ changeCripto, closeOrOpenModalCripto }, dispatch);
+const mapDispatchToProps =
+dispatch => bindActionCreators({ changeCripto, closeOrOpenModalCripto }, dispatch);
+
+ModalCurrency.propTypes = {
+  modalChangeCripto: PropTypes.string.isRequired,
+  changeCripto: PropTypes.func.isRequired,
+  closeOrOpenModalCripto: PropTypes.func.isRequired,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ModalCurrency);
