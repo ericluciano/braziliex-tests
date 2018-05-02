bb5a98626ad2c83f600a79655497383a7791c9b8
Eric Luciano <ericluciano@MacBook-Pro-de-Eric.local>
2018-05-01 21:56:55 -0300

    correção de código e add prop-types

--- src/components/tabs/buttons.jsx
+++ src/components/tabs/buttons.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
@@ -7,6 +9,7 @@ import { changeCripto, tabsNavegarPara } from '../../reducers/criptoActions';
 class Buttons extends React.Component {
   constructor(props) {
     super(props);
+    this.callClick = this.callClick.bind(this);
   }
 
   callOrNotReloadHistorico(props) {
@@ -15,22 +18,39 @@ class Buttons extends React.Component {
     }
   }
 
+  callClick() {
+    this.props.tabsNavegarPara(this.props.action);
+    this.callOrNotReloadHistorico(this.props.mark);
+  }
+
   render() {
     return (
-      <button class={`tablinks ${this.props.active}`} onClick={() => {
-        this.props.tabsNavegarPara(this.props.action)
-        this.callOrNotReloadHistorico(this.props.mark)
-      } }>
+      <button
+        className={`tablinks ${this.props.active}`}
+        onClick={this.callClick}
+        onKeyPress={this.callClick}
+      >
         {this.props.tabTitle}
       </button>
-    )
+    );
   }
 }
 
 const mapStateToProps = state => ({
-  moedaAtual: state.cripto.moedaAtual
+  moedaAtual: state.cripto.moedaAtual,
 });
-const mapDispatchToProps = dispatch =>
-bindActionCreators({ changeCripto, tabsNavegarPara }, dispatch);
 
-export default connect(mapStateToProps,mapDispatchToProps)(Buttons);
+const mapDispatchToProps =
+dispatch => bindActionCreators({ changeCripto, tabsNavegarPara }, dispatch);
+
+Buttons.propTypes = {
+  mark: PropTypes.string.isRequired,
+  moedaAtual: PropTypes.string.isRequired,
+  tabTitle: PropTypes.string.isRequired,
+  active: PropTypes.string.isRequired,
+  action: PropTypes.string.isRequired,
+  changeCripto: PropTypes.func.isRequired,
+  tabsNavegarPara: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Buttons);
