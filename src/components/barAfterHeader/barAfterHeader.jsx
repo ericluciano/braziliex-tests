81867c965e0eb895f1895499314e836b59775cb6
Eric Luciano <ericluciano@MacBook-Pro-de-Eric.local>
2018-05-01 21:57:49 -0300

    correção de código e add prop-types

--- src/components/barAfterHeader/barAfterHeader.jsx
+++ src/components/barAfterHeader/barAfterHeader.jsx
@@ -1,38 +1,55 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const barAfterHeader = props => {
-  return (
-    <div className="bar-after-header">
-      <div className="box-num-cvt">
-        <span className="i-compra">
-          <i className="far fa-fw fa-circle label-buy label-spacing"></i>
-          {props.compra}
-        </span>
-        <span className="i-venda">
-          <i className="far fa-fw fa-circle label-sell label-spacing"></i>
-          {props.venda}
-        </span>
-        <span className="i-total">
-          <i className="fa fa-fw fa-globe label-total"></i>
-          {props.total}
-        </span>
-      </div>
-      <div className="box-total-cvt">
-        <span className="total-compra">
-          <i className="far fa-fw fa-circle label-buy label-spacing"></i>
-          {props.totalCompra}
-        </span>
-        <span className="total-venda">
-          <i className="far fa-fw fa-circle label-sell label-spacing"></i>
-          {props.totalVenda}
-        </span>
-        <span className="total-montante">
-          <i className="fa fa-fw fa-globe label-total"></i>
-          {props.totalMontante}
-        </span>
-      </div>
+const barAfterHeader = props => (
+  <div className="bar-after-header">
+    <div className="box-num-cvt">
+      <span className="i-compra">
+        <i className="far fa-fw fa-circle label-buy label-spacing" />
+        {props.compra}
+      </span>
+      <span className="i-venda">
+        <i className="far fa-fw fa-circle label-sell label-spacing" />
+        {props.venda}
+      </span>
+      <span className="i-total">
+        <i className="fa fa-fw fa-globe label-total" />
+        {props.total}
+      </span>
     </div>
-  )
-}
+    <div className="box-total-cvt">
+      <span className="total-compra">
+        <i className="far fa-fw fa-circle label-buy label-spacing" />
+        {props.totalCompra}
+      </span>
+      <span className="total-venda">
+        <i className="far fa-fw fa-circle label-sell label-spacing" />
+        {props.totalVenda}
+      </span>
+      <span className="total-montante">
+        <i className="fa fa-fw fa-globe label-total" />
+        {props.totalMontante}
+      </span>
+    </div>
+  </div>
+);
+
+barAfterHeader.defaultProps = {
+  compra: 0,
+  venda: 0,
+  total: 0,
+  totalCompra: 0,
+  totalVenda: 0,
+  totalMontante: 0,
+};
+
+barAfterHeader.propTypes = {
+  compra: PropTypes.number,
+  venda: PropTypes.number,
+  total: PropTypes.number,
+  totalCompra: PropTypes.number,
+  totalVenda: PropTypes.number,
+  totalMontante: PropTypes.number,
+};
 
 export default barAfterHeader;
