d7ba6ee54ede745c58c8ae36440b517aa27094e3
Eric Luciano <ericluciano@MacBook-Pro-de-Eric.local>
2018-05-01 21:57:30 -0300

    correção de código

--- src/components/header/iconTimer.jsx
+++ src/components/header/iconTimer.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from 'react';
+
 const imageSVG = require('../../icons/timer2.svg');
 
-const iconTimer = () => {
-  return (
-    <div className="open-timer-icon">
-      <img src={imageSVG} alt="" width="40" height="40"/>
-    </div>
-  )
-};
+const iconTimer = () => (
+  <div className="open-timer-icon">
+    <img src={imageSVG} alt="" width="40" height="40" />
+  </div>
+);
 
 export default iconTimer;
