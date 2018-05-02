83bdb1cc79f42fb3153f0930323793ba44e25494
Eric Luciano <ericluciano@MacBook-Pro-de-Eric.local>
2018-05-01 21:57:02 -0300

    correção de código e add prop-types

--- src/components/tabs/braziliexTab.jsx
+++ src/components/tabs/braziliexTab.jsx
@@ -1,37 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { connect } from 'react-redux';
+import 'react-table/react-table.css';
+import ReactTable from 'react-table';
 
 import { formatCurrencyToBr } from '../../utils/utils';
 
-// Import React Table
-import ReactTable from "react-table";
-import "react-table/react-table.css";
-
 class BraziliexTab extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-  getImages(name) {
-
-    const url = `https://braziliex.com/img/logos`;
+  static getImages(name) {
+    const url = 'https://braziliex.com/img/logos';
 
     const images = {
       btc_brl: {
         logo: `${url}/btc_25w.png`,
-        text: 'Bitcoin'
+        text: 'Bitcoin',
       },
       eth_brl: {
         logo: `${url}/eth_25w.png`,
-        text: 'Ethereum'
+        text: 'Ethereum',
       },
       bch_brl: {
         logo: `${url}/bch_25w.png`,
-        text: 'BTC Cash'
+        text: 'BTC Cash',
       },
       ltc_brl: {
         logo: `${url}/ltc_25w.png`,
-        text: 'LiteCoin'
+        text: 'LiteCoin',
       },
       xrp_brl: {
         logo: `${url}/xrp_25w.png`,
@@ -43,55 +38,55 @@ class BraziliexTab extends React.Component {
       },
       xmr_brl: {
         logo: `${url}/xmr_25w.png`,
-        text: 'Monero'
+        text: 'Monero',
       },
       dash_brl: {
         logo: `${url}/dash_25w.png`,
-        text: 'Dash'
+        text: 'Dash',
       },
       btg_brl: {
         logo: `${url}/btg_25w.png`,
-        text: 'BTC Gold'
+        text: 'BTC Gold',
       },
       sngls_brl: {
         logo: `${url}/sngls_25w.png`,
-        text: 'Singular'
+        text: 'Singular',
       },
       gnt_brl: {
         logo: `${url}/gnt_25w.png`,
-        text: 'Golem'
+        text: 'Golem',
       },
       zec_brl: {
         logo: `${url}/zec_25w.png`,
-        text: 'ZCash'
+        text: 'ZCash',
       },
       dcr_brl: {
         logo: `${url}/dcr_25w.png`,
-        text: 'Decred'
+        text: 'Decred',
       },
       mxt_brl: {
         logo: `${url}/mxt_25w.png`,
-        text: 'Martex Coin'
+        text: 'Martex Coin',
       },
       eos_brl: {
         logo: `${url}/eos_25w.png`,
-        text: 'EOS'
+        text: 'EOS',
       },
       crw_brl: {
         logo: `${url}/crw_25w.png`,
-        text: 'Crow'
+        text: 'Crow',
       },
       omg_brl: {
         logo: `${url}/omg_25w.png`,
-        text: 'OmiseGo'
+        text: 'OmiseGo',
       },
       bnb_brl: {
         logo: `${url}/bnb_25w.png`,
-        text: 'Binance'
+        text: 'Binance',
       },
     };
 
-    if(images[name] === undefined) {
+    if (images[name] === undefined) {
       return name;
     }
 
@@ -99,20 +94,23 @@ class BraziliexTab extends React.Component {
       <div>
         <img src={images[name].logo} alt={images[name].text} title={images[name].text} />
       </div>
-    )
+    );
   }
 
-  processMountData(props) {
+  static processMountData(props) {
     const data = props || [];
 
-    const items = Object.values(data).map(i => {
-      return {
-        nome: i['market'],
-        volume: i['quoteVolume24'],
-        preco: i['last'],
-        variacao: i['percentChange']
-      }
+    const items = Object.values(data).map((i) => {
+      const {
+        market, quoteVolume24, last, percentChange,
+      } = i;
 
+      return {
+        nome: market,
+        volume: quoteVolume24,
+        preco: last,
+        variacao: percentChange,
+      };
     });
 
     return items.slice(0, 18);
@@ -121,67 +119,70 @@ class BraziliexTab extends React.Component {
   mountTable() {
     return (
       <ReactTable
-        data={this.processMountData(this.props.currencies)}
+        data={BraziliexTab.processMountData(this.props.currencies)}
         columns={[{
-          Header: "moeda",
-          accessor: "nome",
-          Cell: props => this.getImages(props.value),
-          sortable: false
+          Header: 'moeda',
+          accessor: 'nome',
+          Cell: props => BraziliexTab.getImages(props.value),
+          sortable: false,
         },
         {
-          Header: "preço",
-          accessor: "preco",
+          Header: 'preço',
+          accessor: 'preco',
           Cell: props => formatCurrencyToBr(props.value),
           sortMethod: (a, b) => {
             if (a.length === b.length) {
               return a > b ? 1 : -1;
             }
             return a.length > b.length ? 1 : -1;
-          }
-
+          },
         },
         {
-          Header: "volume",
-          accessor: "volume",
+          Header: 'volume',
+          accessor: 'volume',
           Cell: props => formatCurrencyToBr(props.value),
           sortMethod: (a, b) => {
             if (a.length === b.length) {
               return a > b ? 1 : -1;
             }
             return a.length > b.length ? 1 : -1;
-          }
+          },
         },
         {
-          Header: "variação",
-          accessor: "variacao",
+          Header: 'variação',
+          accessor: 'variacao',
           Cell: props => `${props.value}%`,
           sortMethod: (a, b) => {
             if (a.length === b.length) {
               return a > b ? 1 : -1;
             }
             return a.length > b.length ? 1 : -1;
-          }
-
-        }
+          },
+        },
       ]}
-      defaultPageSize={18}
-      className="-striped -highlight"
-      showPagination={false}
-    />
-  )
-}
+        defaultPageSize={18}
+        className="-striped -highlight"
+        showPagination={false}
+      />
+    );
+  }
 
-render() {
-  return (
-    <div className={`tabcontent ${this.props.status}`}>
-      {this.mountTable()}
-    </div>
-  )
-}
+  render() {
+    return (
+      <div className={`tabcontent ${this.props.status}`}>
+        {this.mountTable()}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-  currencies: state.cripto.currencies
+  currencies: state.cripto.currencies,
 });
+
+BraziliexTab.propTypes = {
+  currencies: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+};
 
 export default connect(mapStateToProps)(BraziliexTab);
